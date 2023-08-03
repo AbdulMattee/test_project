1b78a20f006c044566586288f3a016c03bece8b5
AbdulMattee <88973154+AbdulMattee@users.noreply.github.com>
2023-08-03 13:25:15 +0500

    migration added of STOCK table

---
+++ server/src/database/migrations/20230803081423_stocks.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  return knex.schema.createTable("stocks", (table) => {
+    table.integer("stockId").notNullable();
+    table.string("sku").notNullable();
+    
+    table.timestamps(true, true);
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  return knex.schema.dropTableIfExists("stocks");
+}
